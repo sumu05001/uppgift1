e7ecbe5d270ef3de4946e4bae671c630d864330a
Admin <Admin@TEB127001>
2023-01-18 10:28:01 +0100

    Add project files.

---
+++ uppgift1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel (1)");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som som \" och \\");
+        }
+    }
+}
+// Jag klar 
